2f9eac5fc4235c67a1ba81a01508428942cfa49e
21Esteban <jelc200521@gmail.com>
2024-04-01 22:04:24 -0500

    funciones crear estructura

--- parcial/funciones.h
+++ parcial/funciones.h
@@ -1,4 +1,13 @@
 #ifndef FUNCIONES_H
 #define FUNCIONES_H
 
+#include <iostream>
+
+using namespace std;
+
+//prototipos
+
+void pedirDatos();
+void crearEstructura();
+
 #endif // FUNCIONES_H
--- parcial/main.cpp
+++ parcial/main.cpp
@@ -1,11 +1,79 @@
-#include <iostream>
+#include"funciones.h"
 
-using namespace std;
+//Variables Globales
+
+int **punteroMatriz,tamaño;
+
 
 int main()
 {
-    cout << "Hello World!" << endl;
+
+    //pedimos los datos para rellenar la estructura M
+    pedirDatos();
+    //creamos la estructura M
+    crearEstructura();
+
+
+
     return 0;
     
 
 }
+
+
+void pedirDatos() {
+    bool datoCorrecto = false;
+
+    while (!datoCorrecto) {
+        cout << "Ingrese el tamano de la estructura M (desde 3x3 hasta 9x9 el numero debe ser impar): ";
+        cin >> tamaño;
+
+        //validacion
+        if (tamaño % 2 == 1 && tamaño >= 3 && tamaño <= 9) {
+            datoCorrecto = true;
+        } else {
+
+            cout << "Digite un valor correcto (3, 5, 7, 9)" << endl;
+        }
+    }
+
+
+}
+
+
+void crearEstructura(){
+
+    punteroMatriz = new int*[tamaño]; //reservamos memoria para las filas
+    for(int i = 0 ;i < tamaño ; i++){
+        punteroMatriz[i] = new int [tamaño];//reservamos memoria para las columnas
+    }
+
+}
+
+
+void llenarEstructura(){
+
+    int variableDeLLenado = 1;
+
+    for(int i = 0 ; i <tamaño ; i++){
+        for(int j = 0; j < tamaño ; j++){
+
+            if(i == (tamaño/2) + 1 && j == (tamaño/2) + 1){
+                punteroMatriz[i][j]=0;
+
+            }else{
+            punteroMatriz[i][j]=variableDeLLenado;
+            variableDeLLenado++;
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
